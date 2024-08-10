6f8c079b7c73ca888ab9b71534b08370685b0e6d
Ruan Dias <ruandiasat@gmail.com>
2024-08-10 12:12:09 -0300

    ex 008, 009 and 010 added

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if (vel > 60) {
+    console.log("Você passou acima da velocidade permitida! MULTADO!")
+}
+console.log("Dirija sempre usando o cinto de segurança")
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+pais = 'Brasil'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil') {
+    console.log("Brasileiro")
+} else {
+    console.log("Gringo")
+}
