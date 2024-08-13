56ad78d5e0860c77bf416cf205a4ddcaa99644b7
Ruan Dias <ruandiasat@gmail.com>
2024-08-13 10:12:13 -0300

    Create ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,13 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+// console.log("Tudo Bem")
+// console.log("Tudo Bem")
+// console.log("Tudo Bem")
+// console.log("Tudo Bem")
+// console.log("Tudo Bem")
+// console.log("Tudo Bem")
